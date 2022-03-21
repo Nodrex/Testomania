c641d8deceb11ffe77c5e03f44a17a52b13be988
Nodar_Tchumbadze <nchumbadze@gmail.com>
2022-03-21 10:48:45 +0400

    Converted project to compose

--- testomania/src/main/java/com/earth/testomania/MainActivity.kt
+++ testomania/src/main/java/com/earth/testomania/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.earth.testomania
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.earth.testomania.ui.theme.TestomaniaTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            TestomaniaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("World!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Testomania $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestomaniaTheme {
+        Greeting("Android")
     }
 }
