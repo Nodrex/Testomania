79c092fde36cbeca1f9d22a1c27e596b26b5073c
shalva97 <shalva258@gmail.com>
2022-04-10 17:19:56 +0400

    add letter spacing use letterSpacing instead of spaces

--- testomania/src/main/java/com/earth/testomania/presentation/TopBar.kt
+++ testomania/src/main/java/com/earth/testomania/presentation/TopBar.kt
@@ -1,20 +1,20 @@
 package com.earth.testomania
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.ExperimentalUnitApi
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 
 @OptIn(ExperimentalUnitApi::class)
 @Preview
@@ -29,7 +29,8 @@ fun TopBar() {
             contentAlignment = Alignment.CenterStart,
         ) {
             Text(
-                text = "T e s t o m a n i a",
+                text = stringResource(id = R.string.title_name),
+                letterSpacing = 10.sp,
                 fontWeight = FontWeight.Bold,
                 fontSize = TextUnit(24f, TextUnitType.Sp),
                 textAlign = TextAlign.Center
