4f918662f6fdbc8bb823fd83973b57002faf6589
shalva97 <shalva258@gmail.com>
2022-04-10 14:30:44 +0400

    remove comment

--- testomania/src/main/java/com/earth/testomania/presentation/skillz/SkillzTestsScreen.kt
+++ testomania/src/main/java/com/earth/testomania/presentation/skillz/SkillzTestsScreen.kt
@@ -12,12 +12,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.spec.DestinationStyle
 
 @OptIn(ExperimentalAnimationApi::class)
 @Destination(
-    route = "home/skillz",
-//    style = DestinationStyle.BottomSheet::class
+    route = "home/skillz"
 )
 @Composable
 @Preview
