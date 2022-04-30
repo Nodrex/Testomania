24d7c9700fa5adbb49598aaf94b942716b60c832
Nodar_Tchumbadze <nchumbadze@gmail.com>
2022-04-30 13:52:37 +0400

    Some UI changes

--- testomania/src/main/java/com/earth/testomania/technical/presentation/ui_parts/QuizAnswerUI.kt
+++ testomania/src/main/java/com/earth/testomania/technical/presentation/ui_parts/QuizAnswerUI.kt
@@ -7,57 +7,65 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.earth.testomania.R
+import kotlin.random.Random
 
 @Composable
 fun CreateQuizAnswerUI() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 10.dp, end = 10.dp)
-            .border(
-                border = BorderStroke(1.5.dp, Color.Black),
-                shape = RoundedCornerShape(10.dp)
-            )
-            .padding(all = 12.dp),
-    ) {
-        Box(
-            Modifier
+    Box {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp, top = 8.dp)
                 .border(
                     border = BorderStroke(1.5.dp, Color.Black),
                     shape = RoundedCornerShape(10.dp)
                 )
-                .padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 6.dp)
+                .padding(all = 12.dp),
         ) {
-            Text(
-                text = "A"
-            )
-        }
-        Spacer(modifier = Modifier.width(15.dp))
-        //TODO not suited well fro multi line answer, need other ui for that
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
+            Box(
+                Modifier
+                    .border(
+                        border = BorderStroke(1.5.dp, Color.Black),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    .padding(start = 12.dp, end = 12.dp, top = 6.dp, bottom = 6.dp)
+            ) {
+                Text(
+                    text = "A"
+                )
+            }
+            Spacer(modifier = Modifier.width(15.dp))
+
+            //TODO not suited well fro multi line answer, need refactoring for that
             Text(
                 modifier = Modifier
                     .wrapContentSize()
-                    .padding(top = 5.dp),
+                //.padding(top = 5.dp)
+                ,
+                //text = "Some12345678901234567890123456789 possible answer kjsdjjjsd sdjkf sdjfskjdhf jksdhfkhskdjf kjshdfkhskjdf ksjdfkjhsdkjf kjsdhfkjhskjdfh jksbdfkbsjkdbfkjsbdf kjsbdfkjbskfbfk kjsbdffkjsbkfjb"
                 text = "Some possible answer"
             )
-            Icon(
-                painter = painterResource(id = R.drawable.ic_check),
-                contentDescription = null,
-            )
+
         }
+        Icon(
+            modifier = Modifier.align(Alignment.TopEnd),
+            painter = painterResource(id = tmp() /*R.drawable.ic_correct*/),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
     }
     Spacer(modifier = Modifier.height(15.dp))
 }
+
+private fun tmp() =
+    if (Random(System.currentTimeMillis()).nextBoolean()) R.drawable.ic_correct else R.drawable.ic_wrong
 
 @Preview
 @Composable
--- testomania/src/main/java/com/earth/testomania/technical/presentation/ui_parts/QuizInfoUI.kt
+++ testomania/src/main/java/com/earth/testomania/technical/presentation/ui_parts/QuizInfoUI.kt
@@ -1,14 +1,10 @@
 package com.earth.testomania.technical.presentation.ui_parts
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,17 +15,12 @@ fun CreateQuizInfoUI() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .border(
-                border = BorderStroke(1.5.dp, Color.Black),
-                shape = RoundedCornerShape(20.dp)
-            )
             .padding(all = 10.dp)
     ) {
         Icon(painter = painterResource(id = R.drawable.ic_category), contentDescription = null)
         Spacer(modifier = Modifier.width(10.dp))
         Text(text = "Category/SubCategory")
     }
-    Spacer(modifier = Modifier.height(10.dp))
 }
 
 @Preview
