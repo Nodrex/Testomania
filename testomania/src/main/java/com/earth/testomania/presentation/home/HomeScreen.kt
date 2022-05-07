1379648c98532b5ac25d6c3b7ffc1aea1c1ba256
shalva97 <shalva258@gmail.com>
2022-05-07 23:43:55 +0400

    brand new home screen

--- testomania/src/main/java/com/earth/testomania/presentation/home/HomeScreen.kt
+++ testomania/src/main/java/com/earth/testomania/presentation/home/HomeScreen.kt
@@ -1,18 +1,21 @@
 package com.earth.testomania.presentation.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.earth.testomania.R
 import com.earth.testomania.presentation.destinations.SkillzTestScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kiwi.orbit.compose.ui.controls.Card
 
 @Destination(
     route = "home",
@@ -23,24 +26,42 @@ import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 fun HomeScreen(
     navigator: DestinationsNavigator? = null,
 ) {
-    Column(Modifier.padding(start = 22.dp)) {
-        Button(
-            onClick = {
-                navigator?.navigate(SkillzTestScreenDestination())
-            }) {
-            Text(text = stringResource(id = R.string.general_skills_tests))
-        }
-        Button(
-            onClick = {
+    Column(Modifier
+        .padding(22.dp)
+        .fillMaxSize(), verticalArrangement = Arrangement.spacedBy(15.dp)) {
 
-            }) {
-            Text(text = stringResource(id = R.string.driving_license_tests))
+        CardButton(R.string.general_skills_tests, R.drawable.ic_math) {
+            navigator?.navigate(SkillzTestScreenDestination())
         }
-        Button(
-            onClick = {
 
-            }) {
-            Text(text = stringResource(id = R.string.geography_tests))
+        CardButton(R.string.driving_license_tests, R.drawable.ic_driver_license) {
+
+        }
+
+        CardButton(R.string.geography_tests, R.drawable.ic_geo) {
+            // navigate
+        }
+    }
+}
+
+@Composable
+fun CardButton(
+    @StringRes text: Int = R.string.general_skills_tests,
+    @DrawableRes icon: Int = R.drawable.ic_math,
+    onClick: () -> Unit,
+) {
+    Card(modifier = Modifier.fillMaxWidth(), onClick = onClick) {
+        Row(Modifier
+            .fillMaxWidth()
+            .padding(10.dp)) {
+            Image(painter = painterResource(id = icon),
+                modifier = Modifier.size(20.dp),
+                contentDescription = null)
+            Text(text = stringResource(id = text),
+                Modifier.padding(start = 10.dp))
+            Image(painter = painterResource(id = R.drawable.ic_forward),
+                modifier = Modifier.size(20.dp),
+                contentDescription = null)
         }
     }
 }
