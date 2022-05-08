bcbc21f99c4dc11b596d3eab4063f9c1022e31dd
shalva97 <shalva258@gmail.com>
2022-05-08 15:11:14 +0400

    change to card views

--- testomania/src/main/java/com/earth/testomania/presentation/home/HomeScreen.kt
+++ testomania/src/main/java/com/earth/testomania/presentation/home/HomeScreen.kt
@@ -1,67 +1,62 @@
 package com.earth.testomania.presentation.home
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.earth.testomania.R
-import com.earth.testomania.presentation.destinations.SkillzTestScreenDestination
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import kiwi.orbit.compose.ui.controls.Card
 
+@Preview
 @Destination(
     route = "home",
     start = true
 )
-@Preview
 @Composable
 fun HomeScreen(
     navigator: DestinationsNavigator? = null,
 ) {
-    Column(Modifier
-        .padding(22.dp)
-        .fillMaxSize(), verticalArrangement = Arrangement.spacedBy(15.dp)) {
 
-        CardButton(R.string.general_skills_tests, R.drawable.ic_math) {
-            navigator?.navigate(SkillzTestScreenDestination())
-        }
+    val viewModel: HomeScreenViewModel = hiltViewModel()
 
-        CardButton(R.string.driving_license_tests, R.drawable.ic_driver_license) {
-
-        }
-
-        CardButton(R.string.geography_tests, R.drawable.ic_geo) {
-            // navigate
+    @OptIn(ExperimentalFoundationApi::class)
+    LazyVerticalGrid(cells = GridCells.Fixed(2),
+        contentPadding = PaddingValues(10.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        items(viewModel.destinations.size) { index ->
+            val item = viewModel.destinations[index]
+            CardButton(item)
         }
     }
 }
 
 @Composable
 fun CardButton(
-    @StringRes text: Int = R.string.general_skills_tests,
-    @DrawableRes icon: Int = R.drawable.ic_math,
-    onClick: () -> Unit,
+    destinationInfo: HomeDestinations,
+    navigator: DestinationsNavigator? = null,
 ) {
-    Card(modifier = Modifier.fillMaxWidth(), onClick = onClick) {
-        Row(Modifier
-            .fillMaxWidth()
-            .padding(10.dp)) {
-            Image(painter = painterResource(id = icon),
-                modifier = Modifier.size(20.dp),
+    Card(onClick = {
+        navigator?.navigate(destinationInfo.destination)
+    }) {
+        Column(Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painter = painterResource(id = destinationInfo.icon),
+                modifier = Modifier.size(40.dp),
                 contentDescription = null)
-            Text(text = stringResource(id = text),
-                Modifier.padding(start = 10.dp))
-            Image(painter = painterResource(id = R.drawable.ic_forward),
-                modifier = Modifier.size(20.dp),
-                contentDescription = null)
+            Text(text = stringResource(id = destinationInfo.name),
+                Modifier.padding(start = 10.dp), textAlign = TextAlign.Center)
         }
     }
 }
