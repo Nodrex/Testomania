c64f215432e958b2d1bca132a4e5f45a4f2d3da1
nodrex <nchumbadze@gmail.com>
2022-07-03 02:14:13 +0400

    About Bottom Sheet is done for now

---
+++ testomania/src/main/java/com/earth/testomania/core/Developer.kt
@@ -0,0 +1,33 @@
+package com.earth.testomania.core
+
+enum class Developer(
+    val fullName: String,
+    val githubProfileLink: String,
+    val linkedinProfileLink: String
+) {
+    GIORGI_SHALVASHVILI(
+        "Giorgi Shalvashvili",
+        "https://github.com/shalva97",
+        "https://www.linkedin.com/in/giorgi-shalvashvili/"
+    ),
+    KARTLOS_DIAKONIDZE(
+        "Kartlos Diakonidze",
+        "https://github.com/kdiakonidze",
+        "https://www.linkedin.com/in/kartlos-diakonidze-93bb91138/"
+    ),
+    NODAR_TCHUMBADZE(
+        "Nodar Tchumbadze",
+        "https://github.com/Nodrex",
+        "https://www.linkedin.com/in/nodar-tchumbadze-15351163/"
+    ),
+    NIKA_MGALOBLISHVILI(
+        "Nika Mgaloblishvili",
+        "https://github.com/nmgalo",
+        "https://www.linkedin.com/in/nika-mgaloblishvili/"
+    ),
+    LIKA_GLONTI(
+        "Lika Glonti",
+        "https://github.com/likaGlonti",
+        "https://www.linkedin.com/in/lika-glonti-p120/"
+    ),
+}
--- testomania/src/main/java/com/earth/testomania/presentation/AboutBottomSheet.kt
+++ testomania/src/main/java/com/earth/testomania/presentation/AboutBottomSheet.kt
@@ -1,47 +1,82 @@
 package com.earth.testomania.presentation
 
-import androidx.compose.foundation.layout.padding
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
 import com.earth.testomania.R
+import com.earth.testomania.core.Developer
 import kiwi.orbit.compose.ui.controls.ChoiceTile
+import kiwi.orbit.compose.ui.controls.Icon
+
 
 @Composable
 fun CreateAboutBottomSheet() {
 
-    Text(text = stringResource(id = R.string.about_app))
+    val context = LocalContext.current
+    val appGithubUrl = stringResource(id = R.string.app_github_url)
 
-    AboutDeveloper(name = "Giorgi Shalvashvili")
-    AboutDeveloper(name = "Kartlos Diakonidze")
-    AboutDeveloper(name = "Nodar Tchumbadze")
-    AboutDeveloper(name = "Nika mgaloblishvili")
-    AboutDeveloper(name = "Lika Glonti")
+    Text(
+        modifier = Modifier.padding(all = 20.dp),
+        fontSize = 16.sp,
+        text = stringResource(id = R.string.about_app)
+    )
 
-    /*Card(onClick = { }) {
-
-        Text(text = "BottomSheet")
-
-    }*/
-
-    /*Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(all = 10.dp)
+    Row(modifier = Modifier
+        .wrapContentSize()
+        .padding(start = 20.dp, end = 20.dp, bottom = 20.dp)
+        .clickable {
+            startActivity(
+                context,
+                Intent(Intent.ACTION_VIEW, Uri.parse(appGithubUrl)),
+                null
+            )
+        }
     ) {
-        Icon(painter = painterResource(id = R.drawable.ic_explanation),
-            contentDescription = null,
-            tint = Color.Unspecified,
+        Icon(
+            painter = painterResource(id = R.drawable.ic_orbit_github),
+            contentDescription = "",
         )
-        Spacer(modifier = Modifier.width(10.dp))
-        Text(text = "Some information to explain")
-    }*/
+        Text(
+            modifier = Modifier.padding(start = 10.dp),
+            fontSize = 16.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            text = appGithubUrl
+        )
+    }
+
+    Text(
+        modifier = Modifier.padding(start = 12.dp, bottom = 8.dp),
+        fontSize = 14.sp,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        text = stringResource(id = R.string.creator_developers)
+    )
+
+    AboutDeveloper(Developer.GIORGI_SHALVASHVILI)
+    AboutDeveloper(Developer.KARTLOS_DIAKONIDZE)
+    AboutDeveloper(Developer.NODAR_TCHUMBADZE)
+    AboutDeveloper(Developer.NIKA_MGALOBLISHVILI)
+    AboutDeveloper(Developer.LIKA_GLONTI)
 }
 
 @Composable
-fun AboutDeveloper(name: String) {
+fun AboutDeveloper(developer: Developer) {
+
+    val context = LocalContext.current
+
     ChoiceTile(
         modifier = Modifier.padding(
             start = 10.dp,
@@ -51,10 +86,38 @@ fun AboutDeveloper(name: String) {
         selected = false,
         showRadio = false,
         title = {
-            Text(text = name)
-        },
-        onSelect = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = developer.fullName)
 
-        }
+                Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+                    Icon(
+                        modifier = Modifier.clickable {
+                            openBrowser(context, developer.githubProfileLink)
+                        },
+                        painter = painterResource(id = R.drawable.ic_orbit_github),
+                        contentDescription = "",
+                    )
+                    Icon(
+                        modifier = Modifier.clickable {
+                            openBrowser(context, developer.linkedinProfileLink)
+                        },
+                        painter = painterResource(id = R.drawable.ic_orbit_linkedin),
+                        contentDescription = "",
+                    )
+                }
+            }
+        },
+        onSelect = {}
+    )
+}
+
+private fun openBrowser(context: Context, url: String) {
+    startActivity(
+        context,
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)),
+        null
     )
 }
