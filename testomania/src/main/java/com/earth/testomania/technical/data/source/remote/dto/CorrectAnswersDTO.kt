77fe012e07c94864c663e3480a44ada58c86f513
shalva97 <shalva258@gmail.com>
2022-07-17 14:15:28 +0400

    add anotation to mroe dtos

--- testomania/src/main/java/com/earth/testomania/technical/data/source/remote/dto/CorrectAnswersDTO.kt
+++ testomania/src/main/java/com/earth/testomania/technical/data/source/remote/dto/CorrectAnswersDTO.kt
@@ -1,10 +1,13 @@
 package com.earth.testomania.technical.data.source.remote.dto
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CorrectAnswersDTO(
     val answer_a_correct: String?,
     val answer_b_correct: String?,
     val answer_c_correct: String?,
     val answer_d_correct: String?,
     val answer_e_correct: String?,
-    val answer_f_correct: String?
+    val answer_f_correct: String?,
 )
