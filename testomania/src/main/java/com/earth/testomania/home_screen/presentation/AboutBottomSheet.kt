b68dbaa3de3cd81dc578d6a1bba9e6545a82a139
Nodar Tchumbadze <nchumbadze@gmail.com>
2022-10-16 16:43:15 +0400

    Crash handler for Github/LinkedIn buttons (#223)
    * Crash handler for Github/LinkedIn buttons
    
    * Changes according to code review

--- testomania/src/main/java/com/earth/testomania/home_screen/presentation/AboutBottomSheet.kt
+++ testomania/src/main/java/com/earth/testomania/home_screen/presentation/AboutBottomSheet.kt
@@ -5,6 +5,7 @@ package com.earth.testomania.home_screen.presentation
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
@@ -19,9 +20,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat.startActivity
 import com.earth.testomania.R
+import com.earth.testomania.common.Crashlytics
 import com.earth.testomania.common.Developer
 import com.earth.testomania.common.custom_ui_components.DialogCloseAngle
 import com.earth.testomania.common.developers
+import com.earth.testomania.common.log
 import com.ramcosta.composedestinations.annotation.Destination
 import kiwi.orbit.compose.ui.controls.ChoiceTile
 import kiwi.orbit.compose.ui.controls.Icon
@@ -40,7 +43,9 @@ fun AboutBottomSheet(
     val appGithubUrl = stringResource(id = R.string.app_github_url)
 
     Text(
-        modifier = Modifier.padding(all = 20.dp).padding(top = 20.dp),
+        modifier = Modifier
+            .padding(all = 20.dp)
+            .padding(top = 20.dp),
         fontSize = 16.sp,
         text = stringResource(id = R.string.about_app)
     )
@@ -86,7 +91,6 @@ fun AboutBottomSheet(
 
 @Composable
 fun AboutDeveloper(developer: Developer) {
-
     val context = LocalContext.current
 
     ChoiceTile(
@@ -108,16 +112,31 @@ fun AboutDeveloper(developer: Developer) {
                 Text(text = developer.fullName)
 
                 Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+                    val githubTitle = stringResource(id = R.string.github)
+                    val installBrowserForGithub =
+                        stringResource(id = R.string.install_browser, githubTitle)
                     Icon(
                         modifier = Modifier.clickable {
-                            openBrowser(context, developer.githubProfileLink)
+                            openBrowser(
+                                context,
+                                developer.githubProfileLink,
+                                installBrowserForGithub
+                            )
                         },
                         painter = painterResource(id = R.drawable.ic_orbit_github),
                         contentDescription = "",
                     )
+
+                    val linkedInTitle = stringResource(id = R.string.linkedin)
+                    val installBrowserForLinkedIn =
+                        stringResource(id = R.string.install_browser, linkedInTitle)
                     Icon(
                         modifier = Modifier.clickable {
-                            openBrowser(context, developer.linkedinProfileLink)
+                            openBrowser(
+                                context,
+                                developer.linkedinProfileLink,
+                                installBrowserForLinkedIn
+                            )
                         },
                         painter = painterResource(id = R.drawable.ic_orbit_linkedin),
                         contentDescription = "",
@@ -129,10 +148,15 @@ fun AboutDeveloper(developer: Developer) {
     )
 }
 
-private fun openBrowser(context: Context, url: String) {
-    startActivity(
-        context,
-        Intent(Intent.ACTION_VIEW, Uri.parse(url)),
-        null
-    )
+private fun openBrowser(context: Context, url: String, toastText: String) {
+    try {
+        startActivity(
+            context,
+            Intent(Intent.ACTION_VIEW, Uri.parse(url)),
+            null
+        )
+    } catch (e: Exception) {
+        log(e)
+        Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
+    }
 }
