e58dd3010724d4d73931dd5eface516676fc49c5
Vazha Kentchiashvili <vkentchiashvili@gmail.com>
2022-10-22 14:32:53 +0400

    make _error as an read-only shared flow

--- testomania/src/main/java/com/earth/testomania/technical/presentation/QuizViewModel.kt
+++ testomania/src/main/java/com/earth/testomania/technical/presentation/QuizViewModel.kt
@@ -35,7 +35,7 @@ class QuizViewModel @AssistedInject constructor(
     val loading = _loading.asStateFlow()
 
     private val _error = MutableSharedFlow<Int>()
-    val error = _error
+    val error = _error.asSharedFlow()
 
     init {
         getQuizList()
