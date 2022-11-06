582d73646ec73799f1d0de0dffbb08c39e72432d
Nodar Tchumbadze <nchumbadze@gmail.com>
2022-11-06 19:13:11 +0400

    Generic model in progress again

--- testomania/src/main/java/com/earth/testomania/technical/data/repository/QuizRepositoryImpl.kt
+++ testomania/src/main/java/com/earth/testomania/technical/data/repository/QuizRepositoryImpl.kt
@@ -3,8 +3,16 @@ package com.earth.testomania.technical.data.repository
 import com.earth.testomania.common.DataState
 import com.earth.testomania.common.ErrorMetaData
 import com.earth.testomania.common.SuccessMetaData
+import com.earth.testomania.common.log
+import com.earth.testomania.common.model.Answer
+import com.earth.testomania.common.model.Quiz
+import com.earth.testomania.common.model.QuizUIState
+import com.earth.testomania.technical.data.source.remote.QUIZ_API_PATH
 import com.earth.testomania.technical.data.source.remote.QuizApi
+import com.earth.testomania.technical.data.source.remote.dto.TagDTO
 import com.earth.testomania.technical.data.source.remote.dto.TechQuizDTO
+import com.earth.testomania.technical.di.QUIZ_API_BASE_URL
+import com.earth.testomania.technical.domain.model.AnswerKey
 import com.earth.testomania.technical.domain.model.QuizCategory
 import com.earth.testomania.technical.domain.repository.QuizRepository
 import kotlinx.coroutines.flow.Flow
@@ -15,14 +23,142 @@ class QuizRepositoryImpl @Inject constructor(
     private val quizApi: QuizApi
 ) : QuizRepository {
 
-    override suspend fun getQuizList(params: QuizCategory): Flow<DataState<List<TechQuizDTO>>> =
+    override suspend fun getQuizList(params: QuizCategory): Flow<DataState<List<QuizUIState>>> =
         flow {
             quizApi.getQuizList(
                 category = params.category,
                 tags = params.tag
             ).apply {
-                if (isSuccessful) emit(DataState.Success(SuccessMetaData(), payload = body()))
-                else emit(DataState.Error(ErrorMetaData(null)))
+                if (isSuccessful) {
+                    try {
+                        val quizList = mutableListOf<QuizUIState?>()
+                        body()?.forEach {
+                            quizList += convertTechQuizDTOtoDataObject(it)
+                        }
+                        emit(
+                            DataState.Success(
+                                SuccessMetaData(),
+                                payload = quizList.filterNotNull()
+                            )
+                        )
+                    } catch (e: Exception) {
+                        emit(DataState.Error(ErrorMetaData(e)))
+                    }
+                } else emit(DataState.Error(ErrorMetaData(null)))
             }
         }
+
+
+    private fun convertTechQuizDTOtoDataObject(techQuizDTO: TechQuizDTO) = with(techQuizDTO) {
+        val possibleAnswersList = generatePossibleAnswersList(techQuizDTO) ?: return@with null
+
+        val correctAnswersList =
+            generateCorrectAnswersList(techQuizDTO, possibleAnswersList.size) ?: return@with null
+
+        val possibleAnswers = createPossibleAnswerMap(possibleAnswersList)
+        val correctAnswers = createCorrectAnswerMap(correctAnswersList)
+
+        val answers = mutableListOf<Answer>()
+        possibleAnswers.forEach {
+            answers +=
+                Answer(
+                    tag = it.key.first(),
+                    text = it.value,
+                    image = "",
+                    isCorrect = correctAnswers[it.key] ?: false
+                )
+        }
+
+        return@with QuizUIState(
+            Quiz(
+                id = id,
+                point = 1.0,
+                question = question ?: "",
+                category = addTagsToCategory(category, tags),
+                explanation = explanation ?: "",
+                answers = answers
+            )
+        )
+    }
+
+    private fun addTagsToCategory(category: String?, tags: List<TagDTO>?): String {
+        val joinedTags = tags?.mapNotNull {
+            it.takeIf {
+                !(it.name.equals(
+                    category,
+                    ignoreCase = true
+                ))
+            }?.name
+        }?.joinToString(separator = "/")
+        return when {
+            category.isNullOrEmpty() -> joinedTags ?: ""
+            joinedTags.isNullOrEmpty() -> category
+            else -> "$category/$joinedTags"
+        }
+    }
+
+    private fun generatePossibleAnswersList(techQuizDTO: TechQuizDTO) = with(techQuizDTO) {
+        return@with techQuizDTO.answers?.let {
+            return@let listOfNotNull(
+                it.answer_a,
+                it.answer_b,
+                it.answer_c,
+                it.answer_d,
+                it.answer_e,
+                it.answer_f,
+            )
+        }
+    }
+
+    private fun generateCorrectAnswersList(techQuizDTO: TechQuizDTO, trimSize: Int) =
+        with(techQuizDTO) {
+            return@with techQuizDTO.correct_answers?.let {
+                return@let listOfNotNull(
+                    it.answer_a_correct?.toBoolean(),
+                    it.answer_b_correct?.toBoolean(),
+                    it.answer_c_correct?.toBoolean(),
+                    it.answer_d_correct?.toBoolean(),
+                    it.answer_e_correct?.toBoolean(),
+                    it.answer_f_correct?.toBoolean(),
+                ).subList(0, trimSize)
+            }
+        }
+
+    private fun createPossibleAnswerMap(possibleAnswerList: List<String>): Map<String, String> {
+        val map = mutableMapOf<String, String>()
+        possibleAnswerList.forEachIndexed { index, answer ->
+            map[AnswerKey.list[index]] = answer
+        }
+        return map
+    }
+
+    private fun createCorrectAnswerMap(correctAnswersList: List<Boolean>): Map<String, Boolean> {
+        val map = mutableMapOf<String, Boolean>()
+        correctAnswersList.forEachIndexed { index, answer ->
+            map[AnswerKey.list[index]] = answer
+        }
+        return map
+    }
+
+
+    private fun hasCorrectAnswer(techQuizDTO: TechQuizDTO): Boolean {
+        techQuizDTO.correct_answers?.let {
+            val hasCorrectAnswer = it.answer_a_correct.toBoolean() ||
+                    it.answer_b_correct.toBoolean() ||
+                    it.answer_c_correct.toBoolean() ||
+                    it.answer_d_correct.toBoolean() ||
+                    it.answer_e_correct.toBoolean() ||
+                    it.answer_f_correct.toBoolean()
+            if (!hasCorrectAnswer) {
+                log(
+                    "Problematic Quiz (Without correct answer) from: $QUIZ_API_BASE_URL$QUIZ_API_PATH" +
+                            "\n\t$techQuizDTO" +
+                            "\n[Please report to API creators]"
+                )
+            }
+            return hasCorrectAnswer
+        }
+        return false
+    }
+
 }
--- testomania/src/main/java/com/earth/testomania/technical/domain/use_case/GetQuizListUseCase.kt
+++ testomania/src/main/java/com/earth/testomania/technical/domain/use_case/GetQuizListUseCase.kt
@@ -2,6 +2,7 @@ package com.earth.testomania.technical.domain.use_case
 
 import com.earth.testomania.common.*
 import com.earth.testomania.common.model.QuizUIState
+import com.earth.testomania.technical.data.repository.FALSE_STR
 import com.earth.testomania.technical.data.source.remote.QUIZ_API_PATH
 import com.earth.testomania.technical.data.source.remote.dto.TagDTO
 import com.earth.testomania.technical.data.source.remote.dto.TechQuizDTO
@@ -37,84 +38,6 @@ class GetQuizListUseCase @Inject constructor(
             }
         }
 
-    private fun convertTechQuizDTOtoDataObject(techQuizDTO: TechQuizDTO) = with(techQuizDTO) {
-        val possibleAnswersList = generatePossibleAnswersList(techQuizDTO) ?: return@with null
-
-        val correctAnswersList =
-            generateCorrectAnswersList(techQuizDTO, possibleAnswersList.size) ?: return@with null
-
-        return@with TechQuizItemWrapper(
-            TechQuiz(
-                id = id,
-                question = question ?: "",
-                category = addTagsToCategory(category, tags),
-                explanation = explanation ?: "",
-                hasMultiAnswer = multiple_correct_answers?.toBoolean() ?: false,
-                possibleAnswers = createPossibleAnswerMap(possibleAnswersList),
-                correctAnswers = createCorrectAnswerMap(correctAnswersList)
-            )
-        )
-    }
-
-    private fun addTagsToCategory(category: String?, tags: List<TagDTO>?): String {
-        val joinedTags = tags?.mapNotNull {
-            it.takeIf {
-                !(it.name.equals(
-                    category,
-                    ignoreCase = true
-                ))
-            }?.name
-        }?.joinToString(separator = "/")
-        return when {
-            category.isNullOrEmpty() -> joinedTags ?: ""
-            joinedTags.isNullOrEmpty() -> category
-            else -> "$category/$joinedTags"
-        }
-    }
-
-    private fun generatePossibleAnswersList(techQuizDTO: TechQuizDTO) = with(techQuizDTO) {
-        return@with techQuizDTO.answers?.let {
-            return@let listOfNotNull(
-                it.answer_a,
-                it.answer_b,
-                it.answer_c,
-                it.answer_d,
-                it.answer_e,
-                it.answer_f,
-            )
-        }
-    }
-
-    private fun generateCorrectAnswersList(techQuizDTO: TechQuizDTO, trimSize: Int) =
-        with(techQuizDTO) {
-            return@with techQuizDTO.correct_answers?.let {
-                return@let listOfNotNull(
-                    it.answer_a_correct?.toBoolean(),
-                    it.answer_b_correct?.toBoolean(),
-                    it.answer_c_correct?.toBoolean(),
-                    it.answer_d_correct?.toBoolean(),
-                    it.answer_e_correct?.toBoolean(),
-                    it.answer_f_correct?.toBoolean(),
-                ).subList(0, trimSize)
-            }
-        }
-
-    private fun createPossibleAnswerMap(possibleAnswerList: List<String>): Map<String, String> {
-        val map = mutableMapOf<String, String>()
-        possibleAnswerList.forEachIndexed { index, answer ->
-            map[AnswerKey.list[index]] = answer
-        }
-        return map
-    }
-
-    private fun createCorrectAnswerMap(correctAnswersList: List<Boolean>): Map<String, Boolean> {
-        val map = mutableMapOf<String, Boolean>()
-        correctAnswersList.forEachIndexed { index, answer ->
-            map[AnswerKey.list[index]] = answer
-        }
-        return map
-    }
-
     private fun success(list: List<TechQuizDTO>?) =
         DataState.Success(
             SuccessMetaData(),
@@ -126,38 +49,17 @@ class GetQuizListUseCase @Inject constructor(
                 convertTechQuizDTOtoDataObject(techQuiz)
             })
 
-    private fun hasCorrectAnswer(techQuizDTO: TechQuizDTO): Boolean {
-        techQuizDTO.correct_answers?.let {
-            val hasCorrectAnswer = it.answer_a_correct.toBoolean() ||
-                    it.answer_b_correct.toBoolean() ||
-                    it.answer_c_correct.toBoolean() ||
-                    it.answer_d_correct.toBoolean() ||
-                    it.answer_e_correct.toBoolean() ||
-                    it.answer_f_correct.toBoolean()
-            if (!hasCorrectAnswer) {
-                log(
-                    "Problematic Quiz (Without correct answer) from: $QUIZ_API_BASE_URL$QUIZ_API_PATH" +
-                            "\n\t$techQuizDTO" +
-                            "\n[Please report to API creators]"
-                )
-            }
-            return hasCorrectAnswer
-        }
-        return false
-    }
-
     //TODO maybe for later i will move this to extension
     private fun error(dataState: DataState<List<TechQuizDTO>>) = DataState.Error(
         dataState.metaData as ErrorMetaData,
-        emptyList<TechQuizItemWrapper>()
+        emptyList<QuizUIState>()
     )
 
     //TODO maybe for later i will move this to extension
     private fun loading(dataState: DataState<List<TechQuizDTO>>) = DataState.Loading(
         dataState.metaData as LoadingMetaData,
-        emptyList<TechQuizItemWrapper>()
+        emptyList<QuizUIState>()
     )
 
-}
 
-private const val FALSE_STR = "false"
+}
