a381182460dd0e84150be48bb7e80ba450c41eaa
George Shalvashvili <shalva258@gmail.com>
2022-12-11 22:07:03 +0400

    also map correct answers

--- testomania/src/main/java/com/earth/testomania/opentdb/OpenTdbRepoImpl.kt
+++ testomania/src/main/java/com/earth/testomania/opentdb/OpenTdbRepoImpl.kt
@@ -10,16 +10,14 @@ import javax.inject.Inject
 
 interface OpenTdbRepo {
     suspend fun getQuiz(
-        category: OpenTdbCategory,
-        questionCount: Int
+        category: OpenTdbCategory, questionCount: Int
     ): Flow<DataState<List<Quiz>>>
 }
 
 class OpenTdbRepoImpl @Inject constructor(private val api: OpenTdbQuizApi) : OpenTdbRepo {
 
     override suspend fun getQuiz(
-        category: OpenTdbCategory,
-        questionCount: Int
+        category: OpenTdbCategory, questionCount: Int
     ): Flow<DataState<List<Quiz>>> {
         //TODO emit a result
         return flow {
@@ -28,15 +26,24 @@ class OpenTdbRepoImpl @Inject constructor(private val api: OpenTdbQuizApi) : Ope
             )
 
             val quizList = result.body()?.results?.mapIndexed { i: Int, resultDto: ResultDto ->
-                Quiz(
-                    id = i,
+                Quiz(id = i,
                     point = 1.0,
                     question = resultDto.question,
                     category = resultDto.category,
-                    answers = resultDto.incorrect_answers.mapIndexed() { index: Int, answer: String ->
-                        Answer(index.toChar(), answer, null, answer == resultDto.correct_answer)
-                    }
-                )
+                    answers = buildList {
+                        resultDto.incorrect_answers.forEachIndexed { index, answer ->
+                            add(Answer(index.toChar(), answer, null, false))
+                        }
+                        add(
+                            Answer(
+                                tag = count().plus(1).toChar(),
+                                text = resultDto.correct_answer,
+                                image = null,
+                                isCorrect = true
+                            )
+                        )
+                    })
+
             }
 
             emit(DataState.Success(payload = quizList))
@@ -45,27 +52,14 @@ class OpenTdbRepoImpl @Inject constructor(private val api: OpenTdbQuizApi) : Ope
 }
 
 enum class OpenTdbCategory(val id: Int) {
-    GENERAL(9),
-    BOOKS(10),
-    FILM(11),
-    MUSIC(12),
-    MUSICALS_AND_THEATRES(13),
-    TELEVISION(14),
-    VIDEO_GAMES(15),
-    BOARD_GAMES(16),
-    NATURE(17),
-    COMPUTERS(18),
-    MATHEMATICS(19),
-    MYTHOLOGY(20),
-    SPORTS(21),
-    GEOGRAPHY(22),
-    HISTORY(23),
-    POLITICS(24),
-    ART(25),
-    CELEBRITIES(26),
-    ANIMALS(27),
-    VEHICLES(28),
-    COMICS(29),
-    GADGETS(30),
+    GENERAL(9), BOOKS(10), FILM(11), MUSIC(12), MUSICALS_AND_THEATRES(13), TELEVISION(14), VIDEO_GAMES(
+        15
+    ),
+    BOARD_GAMES(16), NATURE(17), COMPUTERS(18), MATHEMATICS(19), MYTHOLOGY(20), SPORTS(21), GEOGRAPHY(
+        22
+    ),
+    HISTORY(23), POLITICS(24), ART(25), CELEBRITIES(26), ANIMALS(27), VEHICLES(28), COMICS(29), GADGETS(
+        30
+    ),
     ANIME_MANGA(31),
 }
