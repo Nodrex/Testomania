527787c714775f87b37345f4c01c81187b002084
Nodar Tchumbadze <nchumbadze@gmail.com>
2023-01-23 00:48:33 +0400

    Network checker is done for now

--- testomania/src/main/java/com/earth/testomania/home_screen/presentation/ui_components/NetworkStateUI.kt
+++ testomania/src/main/java/com/earth/testomania/home_screen/presentation/ui_components/NetworkStateUI.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -61,6 +62,7 @@ fun NetworkStateManager(
                     .fillMaxWidth()
                     .wrapContentHeight(),
                 textAlign = TextAlign.Center,
+                color = Color.White,
                 text = stringResource(R.string.check_your_connection),
                 fontSize = 19.sp,
             )
