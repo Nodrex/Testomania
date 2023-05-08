e2df91c1b57eae25314d01ec0130ec2c0d760ec6
Nodar Tchumbadze <nchumbadze@gmail.com>
2023-05-08 23:34:44 +0400

    Removed unused color variables and converted png to webp

--- testomania/src/main/kotlin/com/earth/testomania/ui/theme/Color.kt
+++ testomania/src/main/kotlin/com/earth/testomania/ui/theme/Color.kt
@@ -7,17 +7,12 @@ val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val Orange = Color(0xFFffbb38)
 val LightGray = Color(0xFFe8e8e8)
 val Gray = Color(0xFF9c9c9c)
 val LightDark = Color(0xFF676767)
 
-val IncorrectBkg = Color(0xfff6f7f9)
-val IncorrectBkgDark = Color(0xff363739)
 val DialogBkgDark = Color(0xFF222222)
 val DialogBkgLight = Color(0xFFEEEEEE)
 
 val LightRed = Color(0xffffb4ab)
 val DarkRed = Color(0xffba1a1a)
-val BottomBarColorDark = Color(0xff262c39)
-val BottomBarColorLight = Color(0xffededee)
